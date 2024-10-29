19031efcf1ca09d02a310c3a4e82972391c03423
Lrid04 <landon.riddik04@gmail.com>
2024-10-29 10:57:44 -0500

    Almost got the models done

--- main.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
-from sqlalchemy.orm import relationship, sessionmaker, declarative_base
-
-engine = create_engine('sqlite:///house')
-Session = sessionmaker(bind=engine)
-
-Base = declarative_base()
---
+++ model/Model.py
@@ -0,0 +1,38 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine, Float, Boolean
+from sqlalchemy.orm import relationship, sessionmaker, declarative_base
+
+engine = create_engine('sqlite:///house')
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+class Room(Base):
+    __tablename__ = 'rooms'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    surface_area = Column(Float)
+    flooring_type = Column(String)
+    flooring_cost_per_sqft = Column(Float)
+
+    is_tiling_needed = Column(Boolean)
+    tile_type = Column(String)
+    tile_cost_per_sqft = Column(Float)
+    tiling_area = Column(Float)
+    total_tile_cost = tiling_area * tile_cost_per_sqft
+    total_flooring_cost = surface_area * flooring_cost_per_sqft
+    total_remodel_cost = total_tile_cost + total_flooring_cost
+    supply = relationship("Supply")
+
+class Supply(Base):
+    __tablename__ = 'supplies'
+    id = Column(Integer, primary_key=True)
+    room_id = Column(Integer, ForeignKey('rooms.id'))
+    name = Column(String)
+    quantity = Column(Integer)
+    cost_per_item = Column(Float)
+    total_supply_cost = quantity * cost_per_item
+
+    room = relationship("Room")
+
+
+Base.metadata.create_all(engine)
