f25330b5153bfcbbdc3f85546c5332f90a5acde7
Lrid04 <landon.riddik04@gmail.com>
2024-10-31 10:19:34 -0500

    small change to main before merge

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
-from sqlalchemy.orm import relationship, sessionmaker, declarative_base
 from flask import Flask, render_template, request, redirect, url_for
-
-# Alchemy Does not need
-# to be here, will be in model
-engine = create_engine('sqlite:///house')
-Session = sessionmaker(bind=engine)
-
-Base = declarative_base()
-
+from model.Model import *
 app = Flask(__name__)
 app.secretkey = 'woah'
 
 
 @app.route('/')
 def index():
-    return render_template('HomePage.html')
+    session = Session()
+    room = session.query(Room).all()
+    return render_template('HomePage.html', room = room)
 
 @app.route('/room')
 def room():
